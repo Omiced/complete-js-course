8e62ca422332fc1f159250d4e104757e9e07631a
Josue Cano <73917299+Omiced@users.noreply.github.com>
2022-09-26 05:44:14 +0000

    top-level await

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -1,5 +1,14 @@
 // importing moduel
-import { addToCart } from './shoppingCart.js';
-console.log('importing module');
+// import { addToCart } from './shoppingCart.js';
+// console.log('importing module');
 
-addToCart('pollo', 1);
+// addToCart('pollo', 1);
+/* ejemplo con top-leven await*/
+const getLastPos = async function () {
+  const res = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+  const data = res.json();
+  return { title: data.at(-1).title, text: data.at(-1).body };
+};
+
+const lastPos = await getLastPos();
+console.log(lastPos);
