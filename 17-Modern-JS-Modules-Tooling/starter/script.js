aaeee099c6b6534bf77402ae3d88a71d259be2e6
Josue Cano <73917299+Omiced@users.noreply.github.com>
2022-10-05 19:42:02 +0000

    finis module pattern example

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -3,7 +3,7 @@
 // console.log('importing module');
 
 // addToCart('pollo', 1);
-/* ejemplo con top-leven await*/
+/* ejemplo con top-leven await
 const getLastPos = async function () {
   const res = await fetch('https://jsonplaceholder.typicode.com/todos/1');
   const data = res.json();
@@ -11,4 +11,30 @@ const getLastPos = async function () {
 };
 
 const lastPos = await getLastPos();
-console.log(lastPos);
+console.log(lastPos);*/
+/* module patern*/
+const ShoppingCart2 = (function () {
+  const cart = [];
+  const shippingCost = 10;
+  const totalPrice = 237;
+  const totalQuantity = 23;
+
+  const addToCart = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(`${quantity} ${product} added to cart`);
+  };
+
+  const orderStock = function (product, quantity) {
+    console.log(`${quantity} ${product} ordered from supplier`);
+  };
+  //cosas que queremos devolver o la API publica.
+  return {
+    addToCart,
+    cart,
+    totalPrice,
+    totalQuantity,
+  };
+})();
+
+ShoppingCart2.addToCart('apple', 4);
+ShoppingCart2.addToCart('pizz', 2);
